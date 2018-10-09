43c322cab8eb71f84d11d6d6d195333d8d983d66
LACHLAN WESTLAKE <102109556@student.swin.edu.au>
2018-10-09 12:27:10 +1100

    Fixed end screen

--- src/EndingGameController.cs
+++ src/EndingGameController.cs
@@ -25,9 +25,12 @@ static class EndingGameController
 
 		if (GameController.HumanPlayer.IsDestroyed) {
 			SwinGame.DrawTextLines("YOU LOSE!", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
+            SwinGame.DrawBitmap(GameResources.GameImage("Lose"), 0, 0);
 		} else {
-			SwinGame.DrawTextLines("-- WINNER --", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
-		}
+            SwinGame.DrawTextLines("-- WINNER --", Color.White, Color.Transparent, GameResources.GameFont("ArialLarge"), FontAlignment.AlignCenter, 0, 250, SwinGame.ScreenWidth(), SwinGame.ScreenHeight());
+            SwinGame.DrawBitmap(GameResources.GameImage("Win"), 0, 0);
+            
+        }
 	}
 
 	/// <summary>
